6ba590dea6be7fdca5ea7da3545d74ecea2ae8da
Makasi-coder <52837230+Makasi-coder@users.noreply.github.com>
2020-05-19 20:08:45 +0300

    Add files via upload
    web testing

---
+++ build/precache-manifest.0ccb32f76f4c0adee37cd6b62ca4d297.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd8c9e1483395c6a8cbe0056fd973c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87833999f0e80964b4d3",
+    "url": "/static/css/2.88550523.chunk.css"
+  },
+  {
+    "revision": "1e1444bd298ea54ac418",
+    "url": "/static/css/main.fe4daff9.chunk.css"
+  },
+  {
+    "revision": "87833999f0e80964b4d3",
+    "url": "/static/js/2.dc68639c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dc68639c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1444bd298ea54ac418",
+    "url": "/static/js/main.c9ae31d9.chunk.js"
+  },
+  {
+    "revision": "cc27b3d9588b7f2c63e3",
+    "url": "/static/js/runtime-main.324bbf50.js"
+  },
+  {
+    "revision": "ac955b1e3a87b3b8f00ad3183b8567e6",
+    "url": "/static/media/manager.ac955b1e.jpg"
+  }
+]);
